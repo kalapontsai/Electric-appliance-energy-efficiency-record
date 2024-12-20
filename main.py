b9e905a2e6ffc9541aa5e24b54b444ee19f48320
kalapontsai <kadelat@gmail.com>
2024-12-20 16:10:56 +0800

    Rev0_3
    #0_3 電力資料以原始資料不用修改表頭,配合溫度檔案時間區段抓取資料
    #0_3 電力值新增可切斷是否同步區段統計或全段統計

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-#rev6 使用sampo_test.ini 來控制輸出參數
+#rev6 使用config.ini 來控制輸出參數
 #rev7 x軸以時間格式讀入, 加入RH POWER子圖是否顯示參數, 修復圖例
 #rev8 檢查csv內是否有足夠的頻道資料
 #rev9 顯示數據統計值,電力能耗值在標題
 #0_1 新增統計數據指定範圍
 #0_2 自動合併日期與時間欄位 from VM7000 記錄檔
 #0_2 以中文字型顯示標題
+#0_3 電力資料以原始資料不用修改表頭,配合溫度檔案時間區段抓取資料
+#0_3 電力值新增可切斷是否同步區段統計或全段統計
 # ***************************************************************
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -39,13 +41,16 @@ except FileNotFoundError:
 	sys.exit()
 
 # 獲取整數值
-Csv_file = config.get('FILE', 'filename')
+Temp_csv_file = config.get('FILE', 'tempfile')
+Power_csv_file = config.get('FILE', 'powerfile')
 Title = config.get('FILE', 'title')
 Fontsize = config.getint('PLT', 'fontsize')
 Stat = config.getint('PLT', 'statistics')
 Stat_range = config.getint('PLT', 'stat_range')
+Stat_range_power = config.getint('PLT', 'stat_range_power')
 Stat_start = config.getint('PLT', 'stat_start')
 Stat_stop = config.getint('PLT', 'stat_stop')
+Show_plt = config.getint('PLT', 'show_plot')
 Xticks = config.getint('PLT', 'xticks')
 Dt_combine = config.getint('CHANNEL', 'DT')
 Channel_str = config.get('CHANNEL', 'temp')
@@ -59,25 +64,35 @@ POWERskip = config.get('CHANNEL', 'POWER_skip')
 # rev 0_2 判斷config內變數[DT] 自動合併日期與時間欄位
 try:
 	if Dt_combine == 0:
-		data = pd.read_csv(config['FILE']['filename'], parse_dates=['DateTime'])
+		data = pd.read_csv(Temp_csv_file, parse_dates=['DateTime'])
 	else:
-		data = pd.read_csv(config['FILE']['filename'])
+		data = pd.read_csv(Temp_csv_file)
 		data['DateTime'] = pd.to_datetime(data['Date'] + ' ' + data['Time'])
-		#print(data['DateTime'])
+	#print(data['DateTime'])
+
+	if Dt_combine == 0 and POWERskip == "0":
+		Power_data = pd.read_csv(Power_csv_file, parse_dates=['DateTime'], skiprows=2)
+	elif Dt_combine == 1 and POWERskip == "0":
+		Power_data = pd.read_csv(Power_csv_file, skiprows=2)
+		Power_data['DateTime'] = pd.to_datetime(Power_data['Date'] + ' ' + Power_data['Time'])
+	
+	#print(Power_data['DateTime'])
+except KeyError as e:
+    print("欄位名稱不正確:",e)
+    print("")
+    input("按Enter後結束程式...")
+    sys.exit()
 except FileNotFoundError:
-    print("csv檔案不存在")
+    print("檔案不存在")
     print("")
     input("按Enter後結束程式...")
     sys.exit()
 
-#檢查csv內是否有INI設定的temp欄位,RH,Volt,I,W
-Channel_check_list = OutputChannel_list + ["RH","Volt","I","W"]
+#檢查csv內是否有INI設定的temp欄位,RH
+Channel_check_list = OutputChannel_list + ["RH"]
 # 部分項目若不顯示,刪除頻道檢查項目
 if POWERskip == "1":
 	fig, ax1 = plt.subplots()
-	Channel_check_list.remove("Volt") #新增檢查csv項目
-	Channel_check_list.remove("I")
-	Channel_check_list.remove("W")
 	Channel_check_list.remove("RH")
 elif RHskip == "1":
 	fig, (ax1, ax3) = plt.subplots(nrows=2, sharex=True, figsize=(10, 6))
@@ -88,48 +103,77 @@ else:
 	plt.subplots_adjust(left=0.15, right=0.85, bottom=0.15, top=0.85, hspace=0.1)
 
 #檢查頻道資料是否存在
-for c in Channel_check_list:
-	if not c in data.columns:
-		print("檔案名稱:",Csv_file)
+for i in Channel_check_list:
+	if not i in data.columns:
+		print("檔案名稱:",Temp_csv_file)
 		print()
 		print("設定顯示頻道",Channel_check_list)
-		print("頻道不存在",c)
+		print("頻道不存在",i)
 		print()
 		input("按Enter後結束程式...")
 		sys.exit()
+
+#rev 0_3
+#若POWERskip == 0,依照溫度檔的時間軸,抓取Power_data的值
+#根據 datetime 尋找最近的 v 值,複製至data[]
+for index, row in data.iterrows():
+	target_time = data.loc[index,'DateTime']
+	# 計算每個 datetime 與目標時間的差值絕對值，並找出差值最小的索引
+	nearest_index = (Power_data['DateTime'] - target_time).abs().idxmin()
+	# 根據索引取得對應的值
+	data.loc[index,'Volt'] = Power_data.loc[nearest_index, 'U(V)']
+	data.loc[index,'I'] = Power_data.loc[nearest_index, 'I(A)']
+	data.loc[index,'W'] = Power_data.loc[nearest_index, 'P(W)']
+	data.loc[index,'WH'] = Power_data.loc[nearest_index, 'WP(Wh)']
 
 #計算統計數據,電耗累積值,並放入標題
 if Stat == 1 and Stat_range != 1:
 	Title += "\n[℃]  AVG  Max   Min"
 	for c in StatChannel_list:
 		if c in Channel_check_list:
-			Title += "\n[" + c + "]  " + str(data[c].mean().round(1)) 
-			Title += "  " + str(data[c].max().round(1)) 
-			Title += "  " + str(data[c].min().round(1))
+			stat_result = "\n[" + c + "]  " \
+			+ str(data[c].mean().round(1)) \
+			+ "  " + str(data[c].max().round(1)) \
+			+ "  " + str(data[c].min().round(1))
+			Title += stat_result
 if Stat == 1 and Stat_range == 1:
 	if isinstance(Stat_start, int) and isinstance(Stat_stop, int) and Stat_start > 0 and ((Stat_stop - 1) <= len(data)) : 
-		Title += "\n[℃]  AVG  Max   Min (" + str(data["DateTime"][Stat_start-1].strftime('%m/%d %H:%M')) + "~" + str(data["DateTime"][Stat_stop-1].strftime('%m/%d %H:%M')) + ")"
+		Title += "\n[℃]  AVG  Max   Min (" + \
+			str(data["DateTime"][Stat_start-1].strftime('%m/%d %H:%M')) + "~" \
+			+ str(data["DateTime"][Stat_stop-1].strftime('%m/%d %H:%M')) + ")"
 		for c in StatChannel_list:
 			if c in Channel_check_list:
-				#print(c,Stat_start-1,Stat_stop-1,data[c].iloc[Stat_start-1:Stat_stop-1].mean())
-				Title += "\n[" + c + "]  " + str(data[c].iloc[Stat_start-1:Stat_stop-1].mean().round(1)) 
-				Title += "  " + str(data[c].iloc[Stat_start-1:Stat_stop-1].max().round(1)) 
-				Title += "  " + str(data[c].iloc[Stat_start-1:Stat_stop-1].min().round(1))
+				stat_result = "\n[" + c + "]  " + str(data[c].iloc[Stat_start-1:Stat_stop-1].mean().round(1)) \
+				+ "  " + str(data[c].iloc[Stat_start-1:Stat_stop-1].max().round(1)) \
+				+ "  " + str(data[c].iloc[Stat_start-1:Stat_stop-1].min().round(1))
+				Title += stat_result
 	else:
 		print("已開啟統計範圍設定,但範圍與資料筆數不符 !!")
 		print("")
 		input("按Enter後繼續...")
-#print(Title)
 
-if POWERskip != "1" and Stat == 1:
-	WH_p = int(data["WH"].max() - data["WH"].min())
-	WH_t = data["DateTime"].iloc[-1] - data["DateTime"].iloc[0]
-	print(WH_p,data["DateTime"].iloc[-1])
+if POWERskip != "1" and Stat == 1 :
+	#判斷是否以區段做計算
+	if Stat_range_power == 1:
+		r_begin = Stat_start
+		r_end = Stat_stop -1
+	else:
+		r_begin = 0
+		r_end = len(data) - 1
+	WH_p = int(data["WH"].iloc[r_end] - data["WH"].iloc[r_begin])
+	WH_t = data["DateTime"].iloc[r_end] - data["DateTime"].iloc[r_begin]
+	#print(WH_p,data["DateTime"].iloc[-1])
 	WH_tmin = int(WH_t.total_seconds() // 60)
 	Title += "\n"
 	Title += str(WH_p) + "W/" + str(WH_tmin) + " min"
 	EF = int((WH_p / WH_tmin) * 1440)
 	Title += " (" + str(EF) + "W/24H)"
+
+if Stat == 1:
+	print(Title)
+
+if Show_plt == 0:
+	sys.exit()
 
 #強制 Matplotlib 使用系統默認的減號符號，而不是從字體中查找
 plt.rcParams['axes.unicode_minus'] = False
@@ -200,15 +244,12 @@ if(POWERskip != "1"):
 	ax31.plot(data['I'], label='I', color='green')
 
 	#ax31.tick_params(axis='y', color='green')
-	ax32.plot(data['Volt'], label='V', color='orange')
 	ax32.spines['right'].set_position(('axes', 1.06))
 	make_patch_spines_invisible(ax32)
 	ax32.spines['right'].set_visible(True)
 	ax32.set_ylabel('Volt', color='orange')
 	ax32.set_ylim(0, 120)
-
-
+	ax32.plot(data['Volt'], label='V', color='orange')
 
 # 显示图形
 plt.show()
-
